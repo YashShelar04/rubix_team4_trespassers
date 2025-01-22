0853d9a0f69fb568565fcf9c6d2b4956f213d5a5
Ashmit-1729 <ashmit.shelke3135@gmail.com>
2025-01-22 09:30:47 +0530

    added accertinity to landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 import SplineComputer from '@/components/SplineComputer';
 import FeatureButton from '@/components/FeatureButton';
+import HomeBackground from "@/components/HomeBackground";
 
 export default function Home() {
   const words = [
@@ -12,27 +13,27 @@ export default function Home() {
 
   return (
     <>
-      <div className="relative w-full h-screen overflow-hidden">
-        {/* Spline Background */}
-          <SplineComputer/>
+      <div className="relative w-full h-[100vh] overflow-hidden">
+        {/* accertinity Background */}
+         <HomeBackground/>
         <div/>
 
         {/* Content Overlay */}
         <div className="relative z-10 w-screen h-full pointer-events-none">
           {/* Login/Signup Button */}
-          <div className="absolute top-0 right-10 pointer-events-auto">
+          <div className="absolute top-5 right-10 pointer-events-auto">
             <button className="text-white bg-purple-700 rounded-xl shadow-lg px-2 py-2 z-20 cursor-pointer">
               Login/Signup
             </button>
           </div>
-          <div className="absolute top-0 left-10 pointer-events-auto">
+          <div className="absolute top-5 left-10 pointer-events-auto">
           <FeatureButton />
           </div>
 
           {/* Typewriter Effect */}
-          <div className="w-full flex justify-center mt-10">
+          {/* <div className="w-full flex justify-center mt-10">
             <TypewriterEffectSmooth words={words} />
-          </div>
+          </div> */}
         </div>
       </div>
     </>
